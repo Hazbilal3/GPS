ffd3419c07c6881248a996c8f0ebec1c0cb918cb
GPS Admin <expeditetransportgps@gmail.com>
2026-04-23 02:46:42 +0500

    Set frontend API to production URL

--- frontend/src/port.interface.ts
+++ frontend/src/port.interface.ts
@@ -1,2 +1,2 @@
-// export const port = "https://api.expeditedtransport.net"  // production
-export const port = import.meta.env.VITE_PORT || "http://localhost:3010"
+export const port = "https://api.expeditedtransport.net"  // production
+// export const port = import.meta.env.VITE_PORT || "http://localhost:3010"
